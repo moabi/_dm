abc0c30b2c1e48d39b78a872d9c167187ce0d636
david Fieffe <moabi31@gmail.com>
2017-05-10 20:00:27 +0200

    Ga

--- _dm/assets/js/custom.js
+++ _dm/assets/js/custom.js
@@ -266,17 +266,7 @@ var webApp = new Vue({
 
     },
     created: function () {
-        $ga_user_id = this.getUsrId; //'UA-6747896-59'
-        $ga_code = this.ga.code;
-
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-        ga('create', $ga_code, 'auto');
-        ga('send', 'pageview');
-        ga('set', 'userId', $ga_user_id);
+        ga('set', 'userId', this.getUsrId);
 
     },
     mounted: function () {
