f1c909ab4b5e6ebec6c402a136c5fd02eaeff4f3
david Fieffe <moabi31@gmail.com>
2017-09-17 01:22:08 +0200

    rm css

--- _dm/functions.php
+++ _dm/functions.php
@@ -429,7 +429,7 @@ function twentyseventeen_scripts() {
 	}
 
 	if(get_page_template_slug() == 'templates/assets.php'){
-	  wp_enqueue_style( 'assets', get_stylesheet_directory_uri().'/assets/css/tpl/assets.css', array(), null );
+
 	  wp_enqueue_script('assets-vue',get_stylesheet_directory_uri().'/assets/js/tpl/assets.js',array('vue-js','jquery'),'1.01',true);
   }
 
