74a4be58912c710c85488ef7be6e9007c1bf90d4
david Fieffe <moabi31@gmail.com>
2017-11-01 08:25:05 +0100

    inc comment form bulma

--- _dm/comment-form.php
+++ _dm/comment-form.php
@@ -3,21 +3,12 @@
 
 	'fields' => apply_filters( 'comment_form_default_fields', array(
 
-		'author' => '<div class="comment-form-author field">' . '<label for="author" class="label">' . __( 'Your Name*' ) . '</label> 
-' . ( $req ? '<span>*</span>' : '' ) .
-
-		            '<input 
+		'author' => '<div class="comment-form-author field">' . '<label for="author" class="label">' . __( 'Nom*' ) .
+		            '</label><input 
 id="author" class="input" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' .
 		            $aria_req . ' /></div>',
 
-		'email' => '<div class="comment-form-email field">' .
-
-		           '<label for="email" class="label">' . __( 'Your Email*' ) . '</label> 
-' .
-
-		           ( $req ? '<span>*</span>' : '' ) .
-
-		           '<input 
+		'email' => '<div class="comment-form-email field"><label for="email" class="label">' . __( 'Email*' ) . '</label><input 
 id="email" class="email input" name="email" type="text" value="' . esc_attr( $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . ' />' . '</div>',
 
 		'url' => ''
@@ -25,9 +16,7 @@ id="email" class="email input" name="email" type="text" value="' . esc_attr( $co
 
 	'comment_field' => '<div class="field">' .
 
-	                   '<label for="comment" class="label" class="label">' . __( 'Message:' ) . '</label>' .
-
-	                   '<textarea id="comment" 
+	                   '<label for="comment" class="label" class="label">' . __( 'Message:' ) . '</label><textarea id="comment" 
 name="comment" cols="45" rows="8" aria-required="true" class="textarea"></textarea>' .
 
 	                   '</div>',
