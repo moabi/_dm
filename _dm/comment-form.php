7a4f066cb1d35cc12c467fee9786e9532ed319e9
david Fieffe <moabi31@gmail.com>
2017-11-01 08:22:58 +0100

    inc comment form bulma

--- _dm/comment-form.php
+++ _dm/comment-form.php
@@ -3,31 +3,34 @@
 
 	'fields' => apply_filters( 'comment_form_default_fields', array(
 
-		'author' => '<p class="comment-form-author">' . '<label for="author" class="label">' . __( 'Your Name*' ) . '</label> ' . ( $req ? '<span>*</span>' : '' ) .
+		'author' => '<div class="comment-form-author field">' . '<label for="author" class="label">' . __( 'Your Name*' ) . '</label> 
+' . ( $req ? '<span>*</span>' : '' ) .
 
 		            '<input 
 id="author" class="input" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' .
-		            $aria_req . ' /></p>',
+		            $aria_req . ' /></div>',
 
-		'email' => '<p class="comment-form-email">' .
+		'email' => '<div class="comment-form-email field">' .
 
-		           '<label for="email" class="label">' . __( 'Your Email*' ) . '</label> ' .
+		           '<label for="email" class="label">' . __( 'Your Email*' ) . '</label> 
+' .
 
 		           ( $req ? '<span>*</span>' : '' ) .
 
-		           '<input id="email" class="email input" name="email" type="text" value="' . esc_attr( $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . ' />' . '</p>',
+		           '<input 
+id="email" class="email input" name="email" type="text" value="' . esc_attr( $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . ' />' . '</div>',
 
 		'url' => ''
 	) ),
 
-	'comment_field' => '<p>' .
+	'comment_field' => '<div class="field">' .
 
 	                   '<label for="comment" class="label" class="label">' . __( 'Message:' ) . '</label>' .
 
 	                   '<textarea id="comment" 
 name="comment" cols="45" rows="8" aria-required="true" class="textarea"></textarea>' .
 
-	                   '</p>',
+	                   '</div>',
 
 	'comment_notes_after' => '',
 	'class_submit'  => 'submit button is-primary',
